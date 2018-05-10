162f5b598d998ae3a43b15984d51475c23af8de1
Manoj Verma <cse.manoj253@gmail.com>
2018-05-10 19:20:20 +0530

    Update email.js

--- config/email.js
+++ config/email.js
@@ -3,16 +3,16 @@ const xoauth2 = require('xoauth2');
 var smtpTransport = require('nodemailer-smtp-transport');
 
 module.exports = {
-    user: 'manoj.v111253@gmail.com',
-    pass: '7607789271',
+    user: '<your email_id>',
+    pass: '<your password>',
 
     transporter: nodemailer.createTransport(smtpTransport({
         service: 'gmail',
         host : 'smtp.gmail.com',
         secureConnection : false,
         auth : {
-          user: 'manoj.v111253@gmail.com',
-          pass: '7607789272',
+           user: '<your email_id>',
+           pass: '<your password>',
         },
 
         tls: {
